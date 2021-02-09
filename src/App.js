ce2be7e5fdd8efb39dfa0b0f58898b2e59a9be9e
Yunyun <celeph4is@gmail.com>
2021-02-08 19:05:32 -0800

    2.8

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleAdd = e => {
     e.preventDefault();
     if (!persons.find(p => p.name === newName)) {
       const newPerson = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(newPerson));
       setNewName('');
+      setNewNumber('');
     } else {
       window.alert(`${newName} is already in phonebook`);
       setNewName('');
@@ -24,15 +27,18 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={({ target }) => setNewName(target.value)} />
+        <div>name: 
+          <input value={newName} onChange={({ target }) => setNewName(target.value)} />
+        </div>
+        <div>number:
+         <input value={newNumber} onChange={({ target }) => setNewNumber(target.value)} />
         </div>
         <div>
           <button type="submit" onClick={e => handleAdd(e)}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <div>{person.name}</div>)}
+        {persons.map(person => <div>{person.name} {person.number}</div>)}
     </div>
   )
 }
