d9f2212db9917cf2e64c1bf07fb275b133153439
Yunyun <celeph4is@gmail.com>
2021-02-09 11:34:31 -0800

    2.11

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Numbers from './Numbers';
 import Search from './Search';
 import AddForm from './AddForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchTerm, setSearchTerm ] = useState('')
+
+  const getAllPersons = () => {
+    axios.get("http://localhost:3001/persons")
+      .then(({ data }) => setPersons(data))
+  }
+
+  useEffect(getAllPersons, [])
 
   const handleAdd = e => {
     e.preventDefault();
