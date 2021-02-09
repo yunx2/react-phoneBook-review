843b9b9d6ed49e5f165c2032756f8d943b439d38
Yunyun <celeph4is@gmail.com>
2021-02-08 18:57:20 -0800

    2.6-2.7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleAdd = e => {
+    e.preventDefault();
+    if (!persons.find(p => p.name === newName)) {
+      const newPerson = {
+        name: newName
+      }
+      setPersons(persons.concat(newPerson));
+      setNewName('');
+    } else {
+      window.alert(`${newName} is already in phonebook`);
+      setNewName('');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={({ target }) => setNewName(target.value)} />
+        </div>
+        <div>
+          <button type="submit" onClick={e => handleAdd(e)}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person => <div>{person.name}</div>)}
     </div>
-  );
+  )
 }
+
 
 export default App;
