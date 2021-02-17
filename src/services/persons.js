dd1297dcc8f9e766accc2da730aad071e969f026
Yunyun <celeph4is@gmail.com>
2021-02-17 11:15:43 -0800

    change apiUrl to '/api/persons'; app now connects to a mongo db

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3001/persons';
+const apiUrl = 'http://localhost:3001/api/persons';
 
 const getAllPersons = () => {
   return axios.get(apiUrl)
